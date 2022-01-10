9555376d4062f77d8f520b891bf7bd85dd897b6b
Deependrasinghdhau <deependrasinghdhau@gmail.com>
2022-01-10 21:57:35 +0530

    added factorial program

---
+++ C_PROGRAM_TO_FIND_FACTORIAL_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    //program started
+
+
+    int n;
+    int fact = 1;
+    printf("ENTER NO : ");
+    scanf("%d",&n);
+    int i,j;
+    
+    //calculating factorial
+    for(i=1;i<=n;i++);
+    {
+        fact;
+    }
+}
